af394d02cbeac3f5ba2578141f425ffb8072ccd8
eduardo <eduardo.borges.ferraz@gmail.com>
2026-02-18 11:10:19 -0300

    Remove repositório aninhado video-client e adiciona arquivos como normais

---
+++ frontend/video-client/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="relative h-full bg-black lg:h-[140vh]">
+      <main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
+      </main>
+    </div>
+  );
+}
