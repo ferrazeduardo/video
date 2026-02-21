5852e1252bdb5604895185d55bfa9b65b298295f
eduardo <eduardo.borges.ferraz@gmail.com>
2026-02-21 11:54:51 -0300

    criacao do header component e imagem do banner

--- frontend/video-client/app/page.tsx
+++ frontend/video-client/app/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="relative h-full lg:h-[140vh]">
-      <main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
+    <div className="relative h-screen overflow-hidden bg-linear-to-b lg:h-[140vh]">
+      <Header />
+      <main className="relative pb-24 pl-4 lg:pl-16">
         <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
           <div className="absolute flex flex-col left-0 top-0 -z-10 h-[95vh] w-screen bg-black">
+            <Image
+              src='/background.jpg'
+              alt="background"
+              fill={true}
+              className="h-[65vh] object-cover object-top lg:h-[95vh]"
+              />
           </div>
 
             <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
