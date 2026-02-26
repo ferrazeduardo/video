66ddb65d04bad5decc5501dc85f773743bd4a2f4
eduardo <eduardo.borges.ferraz@gmail.com>
2026-02-26 00:23:18 -0300

    inicio edicao header

--- frontend/video-client/app/components/Header.tsx
+++ frontend/video-client/app/components/Header.tsx
@@ -1,3 +1,30 @@
 
+import React from "react";
 
-export default function Header() {return (<div></div>)}
+export default function Header() {
+    return (
+        <header
+            className="
+            fixed
+            top-0
+            z-50
+            flex
+            w-full
+            items-center
+            justify-between
+            px-4
+            py-4
+            lg:px-10 
+            lg:py-6"
+
+        >
+            <ul>
+                <li>Home</li>
+                <li>TV Shows</li>
+                <li>Movies</li>
+                <li>Latest</li>
+            </ul>
+        </header>
+    )
+
+}
