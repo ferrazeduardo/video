244577f56acd70c328031d1f06df24a5449522b0
eduardo <eduardo.borges.ferraz@gmail.com>
2026-02-28 11:35:03 -0300

    botoes laterais

--- frontend/video-client/app/components/Header.tsx
+++ frontend/video-client/app/components/Header.tsx
@@ -14,16 +14,25 @@ export default function Header() {
             justify-between
             px-4
             py-4
+            transition-all
             lg:px-10 
             lg:py-6"
 
         >
-            <ul>
-                <li>Home</li>
-                <li>TV Shows</li>
-                <li>Movies</li>
-                <li>Latest</li>
-            </ul>
+            <div className="flex items-center space-x-2 md:space-x-8">
+                <ul className="hidden md:flex md:space-x-4">
+                    <li>Home</li>
+                    <li>TV Shows</li>
+                    <li>Movies</li>
+                    <li>Latest</li>
+                </ul>
+            </div>
+            <div className="flex items-center space-x-4">
+                <p className="hidden cursor-not-allowed lg:inline">Usuario</p>
+                <img  src='/usuario.jpg' className="cursor-pointer rounded"/>
+                <button className="flex items-center space-x-2 bg-white py-2 px-4 rounded text-black text-sm font-semibold">test</button>
+                <button className="flex items-center space-x-2 bg-white py-2 px-4 rounded text-black text-sm font-semibold">test</button>
+            </div>
         </header>
     )
 
