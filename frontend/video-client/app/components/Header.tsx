d874ad78d74d1080bb591426834f2eed7137c181
eduardo <eduardo.borges.ferraz@gmail.com>
2026-02-28 11:54:35 -0300

    use effect barra de menu

--- frontend/video-client/app/components/Header.tsx
+++ frontend/video-client/app/components/Header.tsx
@@ -1,11 +1,25 @@
 
-import React from "react";
+"use client";  
+import React, { useEffect, useState } from "react";
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleSchroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true)
+            } else {
+                setIsScrolled(false)
+            }
+        };
+        window.addEventListener('scroll', handleSchroll);
+        return () => window.removeEventListener('scroll', handleSchroll);
+    }, [])
+
     return (
         <header
-            className="
-            fixed
+            className={`${isScrolled && 'bg-black'}  fixed
             top-0
             z-50
             flex
@@ -16,7 +30,8 @@ export default function Header() {
             py-4
             transition-all
             lg:px-10 
-            lg:py-6"
+            lg:py-6`}
+           
 
         >
             <div className="flex items-center space-x-2 md:space-x-8">
@@ -29,9 +44,9 @@ export default function Header() {
             </div>
             <div className="flex items-center space-x-4">
                 <p className="hidden cursor-not-allowed lg:inline">Usuario</p>
-                <img  src='/usuario.jpg' className="cursor-pointer rounded"/>
                 <button className="flex items-center space-x-2 bg-white py-2 px-4 rounded text-black text-sm font-semibold">test</button>
                 <button className="flex items-center space-x-2 bg-white py-2 px-4 rounded text-black text-sm font-semibold">test</button>
+                <img src='/usuario.jpg' className="cursor-pointer rounded" />
             </div>
         </header>
     )
