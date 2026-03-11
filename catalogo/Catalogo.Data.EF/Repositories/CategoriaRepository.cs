05c5de80f405c554cce87febdf0c50e9c4e15704
eduardo <eduardo.borges.ferraz@gmail.com>
2026-03-11 15:05:49 -0300

    insert categoria

---
+++ catalogo/Catalogo.Data.EF/Repositories/CategoriaRepository.cs
@@ -0,0 +1,42 @@
+using System;
+using Catalogo.Application.Interface.Repository;
+using Catalogo.Application.Interface.SearchRepository;
+using Catalogo.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo.Data.EF.Repositories;
+
+public class CategoriaRepository : ICategoriaRepository
+{
+    private readonly CatalogoDbContext _context;
+    private DbSet<Categoria> _categorias => _context.Set<Categoria>();
+    public CategoriaRepository(CatalogoDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task Delete(Guid id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Categoria> Get(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Insert(Categoria objeto, CancellationToken cancellationToken)
+    {
+        await _categorias.AddAsync(objeto, cancellationToken);
+    }
+
+    public Task<SearchOutput<Categoria>> Search(SearchInput searchInput, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(Categoria categoria, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
