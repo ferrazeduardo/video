88d9d3b3ee8e4f9d9b68971c2316f463fd31396d
eduardo <eduardo.borges.ferraz@gmail.com>
2026-04-03 10:03:49 -0300

    endpoints categoria controller

--- catalogo/Catalogo.Api/Controllers/CategoriaController.cs
+++ catalogo/Catalogo.Api/Controllers/CategoriaController.cs
@@ -1,4 +1,6 @@
 using Catalogo.Application.UseCases.Categoria.Create;
+using Catalogo.Application.UseCases.Categoria.Delete;
+using Catalogo.Application.UseCases.Categoria.Get;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +24,28 @@ namespace Catalogo.Api.Controllers
         {
             var response = await _mediator.Send(createCategoriaInput, cancellationToken);
             return Ok(response);
+        }
+
+        [HttpPost("obter")]
+        public async Task<IActionResult> Get([FromBody] GetCategoriaInput getCategoriaInput, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(getCategoriaInput, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpPost("delete")]
+        public async Task<IActionResult> Delete([FromBody] DeleteCategoriaInput deleteCategoriaInput, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(deleteCategoriaInput, cancellationToken);
+            return Ok(response);
+        }
+
+
+        [HttpPost("update")]
+        public async Task<IActionResult> Update([FromBody] CreateCategoriaInput createCategoriaInput, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(createCategoriaInput, cancellationToken);
+            return Ok(response);    
         }
     }
 }
