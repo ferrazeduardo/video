5b9d3f049afc3bf103f08eb41a7113b7bce0ed7c
eduardo <eduardo.borges.ferraz@gmail.com>
2026-04-04 22:09:22 -0300

    entidade genero

---
+++ catalogo/Catalogo.Domain/Entity/Genero.cs
@@ -0,0 +1,23 @@
+using System;
+using Catalogo.Domain.SeedWork;
+
+namespace Catalogo.Domain.Entity;
+
+public class Genero : AggregationRoot
+{
+    public Genero(string nome)
+    {
+        Nome = nome;
+        dataCriacao = DateTime.UtcNow;
+        Ativo();
+    }
+
+    public string Nome { get; private set; }
+    public string Status { get; private set; }
+    public DateTime dataCriacao { get; private set; }
+
+    public void Ativo()
+    {
+        Status = "S";
+    }
+}
