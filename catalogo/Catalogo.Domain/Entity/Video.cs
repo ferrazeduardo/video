2aa4fab5c9505b0d7b8eaf53dbfd28131a9b64b3
eduardo <eduardo.borges.ferraz@gmail.com>
2026-05-30 12:14:26 -0300

    rating

--- catalogo/Catalogo.Domain/Entity/Video.cs
+++ catalogo/Catalogo.Domain/Entity/Video.cs
@@ -1,4 +1,5 @@
 using System;
+using Catalogo.Domain.Enum;
 using Catalogo.Domain.Exceptions;
 using Catalogo.Domain.SeedWork;
 using Catalogo.Domain.Validation;
@@ -8,7 +9,7 @@ namespace Catalogo.Domain.Entity;
 
 public class Video : AggregationRoot
 {
-    public Video(string titulo, string descricao, bool publicado, int duracao, int anoLancamento)
+    public Video(string titulo, string descricao, bool publicado, int duracao, int anoLancamento,Rating rating)
     {
         Titulo = titulo;
         Descricao = descricao;
@@ -16,6 +17,7 @@ public class Video : AggregationRoot
         DataCriacao = DateTime.Now;
         Duracao = duracao;
         AnoLancamento = anoLancamento;
+        Rating = rating;
     }
 
     public string Titulo { get; private set; }
@@ -24,7 +26,7 @@ public class Video : AggregationRoot
     public DateTime DataCriacao { get; private set; }
     public int Duracao { get; private set; }
     public int AnoLancamento { get; private set; }
-
+    public Rating Rating { get; set; }
     public void Validacao()
     {
         var notificationHandler = new NotificationValidationHandler();
