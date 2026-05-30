2411e58f7180b528b19869b8e7108bfd0d4633d1
eduardo <eduardo.borges.ferraz@gmail.com>
2026-05-30 08:55:08 -0300

    validacao video

--- catalogo/Catalogo.Domain/Entity/Video.cs
+++ catalogo/Catalogo.Domain/Entity/Video.cs
@@ -1,12 +1,38 @@
 using System;
+using Catalogo.Domain.Exceptions;
 using Catalogo.Domain.SeedWork;
+using Catalogo.Domain.Validation;
+using Catalogo.Domain.Validatiors;
 
 namespace Catalogo.Domain.Entity;
 
 public class Video : AggregationRoot
 {
+    public Video(string titulo, string descricao, bool publicado, int duracao, int anoLancamento)
+    {
+        Titulo = titulo;
+        Descricao = descricao;
+        Publicado = publicado;
+        DataCriacao = DateTime.Now;
+        Duracao = duracao;
+        AnoLancamento = anoLancamento;
+    }
+
     public string Titulo { get; private set; }
     public string Descricao { get; private set; }
-
+    public bool Publicado { get; private set; }
     public DateTime DataCriacao { get; private set; }
+    public int Duracao { get; private set; }
+    public int AnoLancamento { get; private set; }
+
+    public void Validacao()
+    {
+        var notificationHandler = new NotificationValidationHandler();
+        var validator = new VideoValidator(this, notificationHandler);
+        validator.Validate();
+        if (notificationHandler.HasErros())
+            throw new ExcecaoDeDominio("Erros de validação", notificationHandler.Erros);
+
+    }
+
 }
--- catalogo/Catalogo.Domain/Exceptions/ExcecaoDeDominio.cs
+++ catalogo/Catalogo.Domain/Exceptions/ExcecaoDeDominio.cs
@@ -1,12 +1,20 @@
 using System;
+using Catalogo.Domain.Validation;
 
 namespace Catalogo.Domain.Exceptions;
 
 public class ExcecaoDeDominio : Exception
 {
+    public IReadOnlyCollection<ValidationError>? _erros { get; }
     public ExcecaoDeDominio(string mensagem) : base(mensagem)
     {
 
+    }
+
+
+    public ExcecaoDeDominio(string? mensagem, IReadOnlyCollection<ValidationError>? errors = null) : base(mensagem)
+    {
+        _erros = errors;
     }
 
     internal static void HaError(bool regra, string mensagem)
