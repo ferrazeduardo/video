f97aee64af54de53c65eb7cae06b897e5ea97011
eduardo <eduardo.borges.ferraz@gmail.com>
2026-06-04 15:59:30 -0300

    metodo ToString para converter do enun Rating para string e argumentException em ToRating para argumentos invalidos.

--- catalogo/Catalogo.Domain/Extensions/RatingExtension.cs
+++ catalogo/Catalogo.Domain/Extensions/RatingExtension.cs
@@ -16,6 +16,22 @@ public static class RatingExtension
             "14" => Rating.Rate14,
             "16" => Rating.Rate16,
             "18" => Rating.Rate18,
+            _ => throw new ArgumentException("Valor de rating inválido.")
+        };
+    }
+
+    public static string ToString(this Rating rating)
+    {
+        return rating switch
+        {
+            Rating.ER => "ER",
+            Rating.L =>  "L",
+            Rating.Rate10 => "10",
+            Rating.Rate12 => "12",
+            Rating.Rate14 => "14",
+            Rating.Rate16 => "16",
+            Rating.Rate18 => "18",
+            _ => throw new ArgumentException("Valor de rating inválido.")
         };
     }
 }
