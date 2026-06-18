bd7d2cd83ad01d427a01c21d00e27ff604794124
eduardo <eduardo.borges.ferraz@gmail.com>
2026-06-17 23:53:19 -0300

    implementacao do usecase CreateVideo

--- catalogo/Catalogo.Application/UseCases/Video/Create/CreateVideo.cs
+++ catalogo/Catalogo.Application/UseCases/Video/Create/CreateVideo.cs
@@ -1,8 +1,28 @@
 using System;
-
+using Catalogo.Application.Interface;
+using Catalogo.Domain.Interface.Repository;
+using MediatR;
+using AppDomain = Catalogo.Domain.Entity;
 namespace Catalogo.Application.UseCases.Video.Create;
 
-public class CreateVideo
+public class CreateVideo : IRequestHandler<CreateVideoInput, CreateVideoOutput>
 {
+    private IVideoRespository _videoRespository;
+    private IUnitOfWork _unitOfWork;
 
+    public CreateVideo(IVideoRespository videoRespository, IUnitOfWork unitOfWork)
+    {
+        _videoRespository = videoRespository;
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<CreateVideoOutput> Handle(CreateVideoInput request, CancellationToken cancellationToken)
+    {
+        var video = new AppDomain.Video(request.titulo, request.descricao, request.publicado, request.duracao, request.anoLancamento, request.rating);
+
+        await _videoRespository.Insert(video, cancellationToken);
+
+        await _unitOfWork.Commit(cancellationToken);
+
+        return new CreateVideoOutput(video.idGuid);
+    }
 }
--- catalogo/Catalogo.Application/UseCases/Video/Create/CreateVideoOutput.cs
+++ catalogo/Catalogo.Application/UseCases/Video/Create/CreateVideoOutput.cs
@@ -3,13 +3,7 @@ using Catalogo.Domain.Enum;
 namespace Catalogo.Application.UseCases.Video.Create;
 
 public record  CreateVideoOutput(
-    Guid id,
-    String titulo,
-    String descricao,
-    int duracao,
-    Rating rating,
-    int anoLancamento,
-    bool publicado
+    Guid id
 )
 {
 
